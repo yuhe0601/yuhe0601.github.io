e777948a70a4bd7ed851e4f9075a3a54f06ca9c6
yuhe0601 <117771703+yuhe0601@users.noreply.github.com>
2026-04-21 06:29:09 +0000

    Deploying to gh-pages from @ yuhe0601/yuhe0601.github.io@b6ebebfc75197e9108d88abfb6def51d3062aefb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "A growing collection of your cool projects.",
+          description: "These are my research projects and ongoing work.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
