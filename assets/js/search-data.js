4703179ac8a72f773ec5d6f56ce3d5992bb1a68a
yuhe0601 <117771703+yuhe0601@users.noreply.github.com>
2026-04-21 08:44:26 +0000

    Deploying to gh-pages from @ yuhe0601/yuhe0601.github.io@cf4f2311ee39e879e30292cedf444353fa55e6de 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -403,12 +403,7 @@ ninja.data = [{
           section: "News",},{id: "news-storytailor-is-accepted-in-cvpr-2026-congratulations-to-jinghao-hu-sparkles-smile",
           title: 'StoryTailor is accepted in CVPR 2026. Congratulations to Jinghao Hu.:sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-automatic-reassembly-of-fragmented-artifacts-based-on-deep-feature-pattern-modeling",
-          title: '⭐ Automatic Reassembly of Fragmented Artifacts Based on Deep Feature Pattern Modeling',
-          description: "We focus on enabling intelligent virtual restoration of cultural relics by addressing cultural bias, cross-modal hallucinations, and excessive imagination in generative models, through a domain knowledge-driven 3D content generation framework that integrates dynamic feature regulation, cultural-context-aware representation learning, multi-task knowledge distillation, and expert-in-the-loop evaluation to ensure fact-consistent and reliable restoration under low-resource conditions.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
+          section: "News",},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
@@ -443,6 +438,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-automatic-reassembly-of-fragmented-artifacts-based-on-deep-feature-pattern-modeling",
+          title: '⭐ Automatic Reassembly of Fragmented Artifacts Based on Deep Feature Pattern Modeling',
+          description: "We focus on enabling intelligent virtual restoration of cultural relics by addressing cultural bias, cross-modal hallucinations, and excessive imagination in generative models, through a domain knowledge-driven 3D content generation framework that integrates dynamic feature regulation, cultural-context-aware representation learning, multi-task knowledge distillation, and expert-in-the-loop evaluation to ensure fact-consistent and reliable restoration under low-resource conditions.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/NSFC2019_project/";
             },},{id: "projects-large-scale-models-for-ceramic-artifact-virtual-restoration-with-suppressed-cultural-bias-and-hallucination",
           title: '⭐ Large-Scale Models for Ceramic Artifact Virtual Restoration with Suppressed Cultural Bias and...',
           description: "We focus on enabling intelligent virtual restoration of cultural relics by addressing cultural bias, cross-modal hallucinations, and excessive imagination in generative models, through a domain knowledge-driven 3D content generation framework that integrates dynamic feature regulation, cultural-context-aware representation learning, multi-task knowledge distillation, and expert-in-the-loop evaluation to ensure fact-consistent and reliable restoration under low-resource conditions.",
