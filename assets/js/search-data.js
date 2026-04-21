c518d3684215256cf77783b39c5b7d6e07d8052c
yuhe0601 <117771703+yuhe0601@users.noreply.github.com>
2026-04-21 08:49:34 +0000

    Deploying to gh-pages from @ yuhe0601/yuhe0601.github.io@f7b2221d5926495997bc5d075e464ecfea3a8ff8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -443,9 +443,9 @@ ninja.data = [{
           description: "We focus on enabling intelligent virtual restoration of cultural relics by addressing cultural bias, cross-modal hallucinations, and excessive imagination in generative models, through a domain knowledge-driven 3D content generation framework that integrates dynamic feature regulation, cultural-context-aware representation learning, multi-task knowledge distillation, and expert-in-the-loop evaluation to ensure fact-consistent and reliable restoration under low-resource conditions.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/NSFC2025_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-large-scale-models-for-ceramic-artifact-virtual-restoration-with-suppressed-cultural-bias-and-hallucination",
+          title: '**Large-Scale Models for Ceramic Artifact Virtual Restoration with Suppressed Cultural Bias and Hallucination**...',
+          description: "We focus on enabling intelligent virtual restoration of cultural relics by addressing cultural bias, cross-modal hallucinations, and excessive imagination in generative models, through a domain knowledge-driven 3D content generation framework that integrates dynamic feature regulation, cultural-context-aware representation learning, multi-task knowledge distillation, and expert-in-the-loop evaluation to ensure fact-consistent and reliable restoration under low-resource conditions.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/Provincial2025_project/";
             },},{id: "teachings-data-science-fundamentals",
