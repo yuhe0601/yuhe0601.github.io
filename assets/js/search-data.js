f424c78c8c7decaa0499c869440688060310e7a9
yuhe0601 <117771703+yuhe0601@users.noreply.github.com>
2026-04-21 06:59:33 +0000

    Deploying to gh-pages from @ yuhe0601/yuhe0601.github.io@6d05d6c8a51c15ab6196f1d26f648fce769222ee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -404,7 +404,7 @@ ninja.data = [{
           title: 'StoryTailor is accepted in CVPR 2026. Congratulations to Jinghao Hu.:sparkles: :smile:',
           description: "",
           section: "News",},{id: "projects-large-scale-models-for-ceramic-artifact-virtual-restoration-with-suppressed-cultural-bias-and-hallucination",
-          title: 'Large-Scale Models for Ceramic Artifact Virtual Restoration with Suppressed Cultural Bias and Hallucination...',
+          title: '⭐ Large-Scale Models for Ceramic Artifact Virtual Restoration with Suppressed Cultural Bias and...',
           description: "We focus on enabling intelligent virtual restoration of cultural relics by addressing cultural bias, cross-modal hallucinations, and excessive imagination in generative models, through a domain knowledge-driven 3D content generation framework that integrates dynamic feature regulation, cultural-context-aware representation learning, multi-task knowledge distillation, and expert-in-the-loop evaluation to ensure fact-consistent and reliable restoration under low-resource conditions.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
