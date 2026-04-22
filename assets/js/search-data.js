33f1f8688a0e4698e4869e496eb4276657c54af6
yuhe0601 <117771703+yuhe0601@users.noreply.github.com>
2026-04-22 12:16:03 +0000

    Deploying to gh-pages from @ yuhe0601/yuhe0601.github.io@0d3a800bc119e507f50ddc2d3f51e00f458ef2c0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-teaching",
+          title: "Teaching",
+          description: "Course materials, schedules, and resources for classes taught.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
